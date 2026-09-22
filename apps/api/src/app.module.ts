f6a32f4103eafb0c2dd258afb0780e2e2a1b872f
JoJoJo-JoJoJo <jonahthwaite@gmail.com>
2026-09-22 17:49:13 +0100

    feat(api): temp commit before DB schema changes

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { HealthModule } from './health/health.module';
+import { AuthModule } from '@thallesp/nestjs-better-auth';
+import { auth } from "./auth/auth";
 
 @Module({
-  imports: [DatabaseModule, HealthModule]
+  imports: [
+    DatabaseModule,
+    HealthModule,
+    AuthModule.forRoot({ auth })
+  ]
 })
 export class AppModule {}
